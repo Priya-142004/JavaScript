9e8d06da5657e218707f9f0ea1a6987da62e4bff
Priya-142004 <priyanivalkar14@gmail.com>
2025-07-16 07:30:12 +0000

    Compariosn

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,17 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1); // is tarah ka comparison hamaesha predictable result
+// console.log("02" > 1); // nhi deta isliye same datatype use kare
+
+console.log(null > 0);  // don't use this, use same datatype
+console.log(null == 0);
+console.log(null >= 0);
+
+// === -> strict check
+
+console.log("2" === 2); // false, coz it also checks datatypes
+
