b476e309db1fbe0e390d74eb33ef56aa1357143b
Priya-142004 <priyanivalkar14@gmail.com>
2025-07-16 07:56:38 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144542
+let accountEmail = "priya@google.com"
+var accountPassword = "12345"
+accountCity = "Pune"
+let accountState;
+
+//accountId = 2 // not allowed coz it is const
+
+accountEmail = "abc@d.com"
+accountPassword = "4321"
+accountCity = "Delhi"
+
+console.log(accountId);
+
+console.table([accountEmail, accountId, accountPassword, accountCity, accountState]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3) // gives error coz we are using nodejs, not browser
+
+console.log(3 
+    +
+    3) // don't write like this, coz code readibility should be high
+
+console.log("Priya")
+
+let name = "priya"
+let age = 21
+let isLoggedIn = false
+
+// datatyes
+// number -> 2 to the power 53
+//bigint
+// string -> declare in " "
+// boolean -> true/false
+// null -> standalone value
+//undefined
+// symbol -> unique
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+console.log(typeof "Priya"); 
+
