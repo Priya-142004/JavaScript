23eb734806a2d802eba2338bcbfd6e297515cf09
Priya-142004 <priyanivalkar14@gmail.com>
2025-07-23 10:36:32 +0000

    control flow

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,28 @@
+//Syntax -->
+
+// switch (key){
+//     case value:
+
+//     break;
+
+//     default:
+//         break;
+// }
+
+const month = 3 // idhar 3 ke alawa "feb" hota to case 1, 2, 3 ke jagah hum "jan", "feb" aise likhte
+
+switch(month){
+    case 1:
+        console.log("Jan");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3: 
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;        
+    
+}
